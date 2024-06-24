0c3b85dcc19924f4b8496161322fcd8f8aa7e3cb
Tharusha Nimsara <c.t.nimsara@gmail.com>
2024-06-24 11:26:31 +0530

    Add project files.

---
+++ DebraWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DebraWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
